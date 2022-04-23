32d785c7064502a0c72daafb18af7290a7878d25
juandavid8a <juandavid8a@gmail.com>
2022-04-23 14:08:19 -0500

    errors added

--- contador_v6.ino
+++ contador_v6.ino
@@ -32,6 +32,7 @@ bool messageUpdate = false;
 bool preGame = false;
 bool posGame = false;
 bool bombGame = false;
+bool countGame = false;
 
 //Message update
 const unsigned long eventInterval = 1000;
@@ -59,7 +60,7 @@ void loop(){
       if (currentTime - previousTime >= eventInterval) {
         Serial.println(CD[1].remaining());
         if(preGame || bombGame){ soundSmall(); }
-        if(posGame){ soundSmallDouble(); }
+        if(posGame){ soundSmallDouble(2); }
         if(CD[1].remaining() == 6){ posGame = true; } 
         previousTime = currentTime;
       }
@@ -72,12 +73,18 @@ void keypadEvent(KeypadEvent eKey){
     case PRESSED:
 
   switch (eKey){
-    case '*': checkPassword(); break;
+    case '*':
+      if(bombGame){
+        checkPassword();
+      }
+      break;
     case '#':
       switch (gameType) {
         case 1:
           Serial.println("COUNTDOWN STOP");
+          soundSmallDouble(10);
           messageUpdate = false;
+          countGame = false;
           preGame = false;
           posGame = false;
           CD[1].stop();
@@ -85,22 +92,35 @@ void keypadEvent(KeypadEvent eKey){
         case 2:
           Serial.println("PASSWORD CLEAR");
           password.reset();
+          soundSmallDouble(2);
           break;  
       }   
       break;
     case 'C':
-      Serial.println("COUNTDOWN");
-      countdownPreGame();
-      gameType = 1;
+      if(bombGame){
+        Serial.println("ERROR: BOMB SUITCASE ACTIVATED");
+      }else{
+        Serial.println("COUNTDOWN");
+        countdownPreGame();
+        gameType = 1;
+      }
       break;
     case 'B':
-      Serial.println("SUITCASE BOMB");
-      gameType = 2;
-      preGame = false;
-      posGame = false;
-      bombGame = true;
-      messageUpdate = true;
-      CD[1].start(60);
+    if(bombGame){
+        Serial.println("ERROR: BOMB SUITCASE ACTIVATED");
+      }else{
+        if(countGame){
+          Serial.println("ERROR: COUNTDOWN ACTIVATED");
+        }else{
+          Serial.println("SUITCASE BOMB");
+          gameType = 2;
+          preGame = false;
+          posGame = false;
+          bombGame = true;
+          messageUpdate = true;
+          CD[1].start(60);        
+        }
+      }
       break;
     default:
       switch (gameType) {
@@ -117,12 +137,15 @@ void checkPassword(){
   if (password.evaluate()){
     Serial.println("SUITCASE BOMB DEACTIVATED");
     CD[1].stop();
+    password.reset();
+    soundSmallDouble(20);
     messageUpdate = false;
     bombGame = false;
     preGame = false;
     posGame = false;
   }else{
     Serial.println("PASSWORD WRONG");
+    soundSmallDouble(2);
     password.reset();
   }
 }
@@ -131,6 +154,7 @@ void countdownPreGame(){
   preGame = true;
   posGame = false;
   bombGame = false;
+  countGame = true;
   messageUpdate = true;
   CD[1].start(5);
 }
@@ -147,18 +171,13 @@ void soundSmall(){
   digitalWrite(12,LOW); 
 }
 
-void soundSmallDouble(){
-  digitalWrite(12,HIGH);
-  delay(10);
-  digitalWrite(12,LOW);
-  delay(80);
-  digitalWrite(12,HIGH);
-  delay(10);
-  digitalWrite(12,LOW);
-  delay(80);
-  digitalWrite(12,HIGH);
-  delay(10);
-  digitalWrite(12,LOW); 
+void soundSmallDouble(int data){
+   for (data; data >= 0; data--){
+      digitalWrite(12,HIGH);
+      delay(10);
+      digitalWrite(12,LOW);
+      delay(80);
+    }
 }
 
 void soundLarge(){
